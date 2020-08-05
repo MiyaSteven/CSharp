9db3568755a8d3f45e27448d94a13d5682595247
Steven Miyauchi <steven.miyauchi@dappify.org>
2020-08-05 13:44:35 -0700

    working version of card dealing, make my own later

---
+++ assignments/DeckOfCards/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DeckOfCards
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Deck firstDeck = new Deck();
+            firstDeck.Shuffle();
+            for (int i = 0; i < 52; i++)
+            {
+                Console.Write("{0, -19}", firstDeck.DealCards());
+                if ((i + 1) % 4 == 0)
+                    Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
