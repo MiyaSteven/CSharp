2494e3e7674f31a679dccea15abdab21aa3279f7
Steven Miyauchi <steven.miyauchi@dappify.org>
2020-08-06 19:08:51 -0700

    demo

---
+++ FirstWebApp/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspIntro
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("")]
+        public ViewResult Index()
+        {
+            return View("Index");
+        }
+
+        [HttpGet("{name}")]
+        public ViewResult Person(string name)
+        {
+            ViewBag.StringList = new List<string>
+            {
+                "Bill",
+                "Bob",
+                "Tony",
+                "Mary",
+                "Jeff"
+            };
+            ViewBag.RealName = "A-aron";
+
+            ViewBag.Name = name;
+
+            return View("Person");
+        }
+
+        [HttpPost("submit")]
+        public IActionResult FormSubmission(string name, string nickname)
+        {
+            ViewBag.Name = name;
+            ViewBag.NickName = nickname;
+
+            if (name == "Eric")
+            {
+                // return Redirect("/gohomeeric");
+                return RedirectToAction("GoAwayEric");
+            }
+            return View("FormSubmission");
+        }
+
+        [HttpGet("gohomeeric")]
+        public ViewResult GoAwayEric()
+        {
+            return View("GoAwayEric");
+        }
+    }
+}
